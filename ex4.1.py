2a297a8954f05c51b4f7cac264907c30ebe4dc52
Damien DeVille <damien@steady.org>
2015-06-27 14:03:54 -0400

    addign exercise 4

---
+++ ex4.1.py
@@ -0,0 +1,42 @@
+# Define the total number of cars in the fleet.
+
+cars = 100
+
+# Define how many people can fit in a car
+
+space_in_a_car = 4.0
+
+# Define the number of available drivers today
+
+drivers = 30
+
+# Define the number of passengers
+
+passengers = 90
+
+# Define the number of cars not driven by subtracting the number of drivers
+# available from the numbrer of cars available.
+
+cars_not_driven = cars - drivers
+
+# Define the number of cars that will be driven as the same as the number of 
+# Drivers for the day.
+
+cars_driven = drivers
+
+# Define the number of people that can be accomodated in the carpool as the 
+# prodcut of the number of cars driven multiplied by the space in a car.
+
+carpool_capacity = cars_driven * space_in_a_car
+
+# Define the average number of people in a car given the number of cars on 
+# the road by dividing the number of passengers by the number of cars driven.
+
+average_passengers_per_car = passengers / cars_driven
+
+print "There are", cars, "cars avaliable."
+print "There are only", drivers, "drivers avaliable."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
