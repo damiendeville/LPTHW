8c0833cbb5376512e39248aa1e84ab535ffef4ad
Damien DeVille <damien@steady.org>
2015-06-27 11:56:41 -0400

    Revert "Example 1"
    This reverts commit b5680bd33dcd8ba9640e8c5cd92ed7fe81592cbb.

--- ex1.py
+++
@@ -1,8 +0,0 @@
-# print "Hello World!"
-# print "Hello Again"
-# print "I like typing this."
-# rint "This is fun."
-# print 'Yay! Printing.'
-# print "I'd much rather you 'not'."
-# print 'I "said" do not touch this.'
-print "Even if this guy is a little bombastic, this might still work"
