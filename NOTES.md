ab769deda0508aac708dc908d2a68b0fbdc89b59
Damien DeVille <damien@steady.org>
2015-06-28 11:47:16 -0400

    exercise 11
    Fun with raw_input
    Also a few additional notes in NOTES.md

--- NOTES.md
+++ NOTES.md
@@ -1,7 +1,12 @@
-## This is a file for tracking the kind of mistakes I make so that I can learn from them.
+## Notes
+----------
+
+This is a file for tracking mistakes I make and questions I have for additional research so that I can learn from them over time without getting bogged down in meantime.
 
 #### Exercise 8
 ----------
+
+When I fist wrote ex08.py I somehow ended up with a tab at the begining of each line.  Not sure how, but python wouldn't execte it because it was an indentation error.
 
 	File "/Users/damien/GitHub/LPTHW/ex9.0.py", line 6
     print "Hear are the days: ", days
@@ -18,9 +23,12 @@ All initial statements lines need to start with no spaces (indentation comes lat
 
 It seems that you can't use an escape character alone at the beginning of a format string
 
-	This works
+This worksf:
+
 	print "%s a %s at the beginning of a line and add the word %s in the sentence with a format string" % ("\tAdd", "tab", "tab")
 	
-	This Doesn't
+This Doesn't:
+
 	print "%s Add a %s at the beginning of a line and add the word %s in the sentence with a format string" % ("\t", "tab", "tab")
 
+Need to investigate why this is the case.
---
+++ ex11.1.py
@@ -0,0 +1,45 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+print "\n"
+print "So, you are %r old, %r tall and %r heavy." % (
+	age, height, weight)
+
+# Study Drills
+# ------------
+# 1. raw_input() takes input from the user.  Optionally, you can specify a prompt
+# raw_input('--->') prints '--->' as a prompt to the user.
+# See:  https://docs.python.org/2/library/functions.html#raw_input
+#
+# 2. Here is another version of the above program with diffent prompts.  Also
+# It uses %s instead of %r so that it prints pretty.
+
+print "\n"
+print "How old are you?"
+age_yrs = raw_input('Enter your age in years: ')
+print "How tall are you?"
+height_ft_in = raw_input('Enter your height in feet and inches: ')
+print "How much do you weigh?"
+weight_lbs = raw_input('Enter your weight in pounds: ')
+print "\n"
+print "So, you are %s old, %s tall and %s heavy." % (
+	age_yrs, height_ft_in, weight_lbs)
+
+# 3. Here is a new form, similar to the first one, asking different questions.
+print "\n"
+print "How many beers have you had?",
+beers = raw_input()
+print "Would you like another?",
+answer = raw_input()
+
+print "\n"
+print "So you've had %s beers and you said \'%s\' to another." % (beers, answer)
+print "\n"
+# 4. In the original code we used %r which is the repr() format string in python 
+# it prints out the strings the way python sees them - i.e. with quotes and since the 
+# single quote character is used as short hand for "feet" it would look to python as 
+# the end of the string so it needs to be escaped.  This is fixed in the second set of 
+# code which uses the format string %s
