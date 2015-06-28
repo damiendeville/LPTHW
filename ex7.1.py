0fde1094f1a6e66254a7adc9a77f39a7e67d3f65
Damien DeVille <damien@steady.org>
2015-06-27 22:13:33 -0400

    exercise 7 and study drills
    Added mistakes.txt to track mistakes

---
+++ ex7.1.py
@@ -0,0 +1,33 @@
+print "Mary had a little lamb."  # prints Mary had a little lamb
+print "It's fleece was white as %s." % 'snow' # prints It's fleece was white as snow
+print "And everywhere that Mary went." # prints And everwhere that Mary went.
+print "." * 10 # what'd that do? - Prints out 10 periods
+
+end1 = "C" # Assigns C to end1
+end2 = "h" # Assigns h to end2
+end3 = "e" # Assigns e to end3
+end4 = "e" # Assigns e to end4 (could have assigned end3 to end4 as well)
+end5 = "s" # Assigns s to end5
+end6 = "e" # Assigns e to end6
+end7 = "B" # Assigns B to end7
+end8 = "u" # Assigns u to end8
+end9 = "r" # Assigns r to end9
+end10 = "g" # Assigns g to end10
+end11 = "e" # Assigns e to end11
+end12 = "r" # Assigns r to end12
+
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 +end6, # Prints Cheese (the comma creates a 
+											  # space and concatonates the next line)
+print end7 + end8 + end9 + end10 + end11 + end12 # Prints Burger 
+
+# Study Drills
+# ------------
+# 1. Added Comments above
+# 2. I didn't make any mistakes this time.
+# 3. I don't use paper, but I'll create a file called Mistakes.txt to track them.
+# 4 & 5 These are good things to remember. When you go to the next exercise, look at the 
+# mistakes you have made and try not to make them in this new one. Remember that everyone 
+# makes mistakes. Programmers are like magicians who fool everyone into thinking they are 
+# perfect and never wrong, but it's all an act. They make mistakes all the time.
+
